68add65cc50483f65625ad7b62427a02ce348991
Adam Phillabaum <adamp@payscale.com>
2013-04-07 13:45:37 -0700

    Making the cookie last for a long time... so we can refresh the browser w/o requiring a login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-YammerWelcome::Application.config.session_store :cookie_store, key: '_yammer-welcome_session'
+YammerWelcome::Application.config.session_store :cookie_store, { 
+	:key => '_yammer-welcome_session',
+	:expire_after => 4.years
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
