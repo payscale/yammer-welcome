0caa25642668601c9dc38ab1119dcb14cf5a663e
Adam Phillabaum <adamp@payscale.com>
2013-08-27 09:46:27 -0700

    Adding some debug lines, because I'm getting an error message, and I don't know where it's from

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,16 @@ class HomeController < ApplicationController
     @profiles = [];
     @company_name = ""
 
+	print "checking for current user\n"
     if current_user
+      print "we have a current user\n"
       yam ||= Yammer::Client.new(:access_token  => current_user.token)
+      print "We have 'yam'. Get users.\n"
       results = yam.get("/api/v1/users") 
+      print "We have users. Start looping\n"
       page = 1
       until results.body.length < 1 do
-        print results.body.length
+        print results.body.length.to_s() + " users fetched\n"
         results.body.each{ |result| @profiles << result }
         results = yam.get("/api/v1/users", page: page)
         page = page+1
